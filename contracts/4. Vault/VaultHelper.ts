dd9fc39bc5b1e43b1f9eb0d0bbfe1f33e6d8c6b0
Fran Rimoldi <francisco.rimoldi@gmail.com>
2022-06-09 18:02:49 +0200

    exploit Vault contract

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -1,13 +1,24 @@
-import { expect } from "chai";
-import { ethers, waffle } from "hardhat";
+import { ethers } from "hardhat";
 
 const helper = async (victim: any) => {
   /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
+    Declaring a storage variable as "private" only prevents other contracts
+    from reading it. But data on a blockchain will always be public!
+
+    Smart contract's storage variables are store in "slots" of 32 bytes,
+    and it'll pack values together if they fit in 32 bytes.
+
+    `Vault` has 2 storage variables `locked` and `password`.
+    `locked` is a `bool`, and it only takes 1 byte.
+    `password` is a `bytes32`, so it takes 32 bytes.
+
+    Therefore =>
+    slot 0: locked
+    slot 1: password
   */
+  const password = ethers.provider.getStorageAt(victim.address, 1);
+
+  await victim.unlock(password);
 };
 
 export default helper;
